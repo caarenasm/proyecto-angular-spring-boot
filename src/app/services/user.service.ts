a297e0745eeebc60f2deecd5a1fa7e0dd8b596ff
caarenasm <andresam98@hotmail.com>
2025-10-02 15:06:39 -0500

    de nuevo otra configuracion para consumir al backend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -10,10 +10,16 @@ export class UserService {
 
   private users: User[] = [];
 
+  private url: string = 'http://localhost:8080/api/users';
+
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<User[]> {
-    //return of(this.users);
-    return this.http.get<User[]>('http://localhost:8080/api/users');
+    return this.http.get<User[]>(this.url);
+  }
+
+  findById(id: number): Observable<User> {
+    //return this.http.get<User>(this.url + '/' + id);
+    return this.http.get<User>(`${this.url}/${id}`)
   }
 }
